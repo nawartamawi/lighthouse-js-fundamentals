12b493dd528885231b800927fdeb5d5b4f9440c4
nawartamawi <nawartamawi@gmail.com>
2016-11-23 02:16:46 +0000

    adding the excersies before the looping example

---
+++ Nawar.js
@@ -0,0 +1,11 @@
+var Nawar = ["Ahmed" ,  "an Iraqi and ","a Photographer","a Volunteer"," in Canada"];
+console.log(Nawar.join(" is "));
+
+Nawar.push("Terrfic");
+console.log(Nawar.join(" "));
+
+x = "He's a pussy who likes to get fucked hard "
+
+Nawar.push(x);
+
+console.log(Nawar.join(" "));
---
+++ ageCalculator.js
@@ -0,0 +1,8 @@
+function ageCalculator (name, yearofBirth, currentYear){
+
+return name + " is " + (currentYear - yearofBirth) + " years old." ;
+
+};
+
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
